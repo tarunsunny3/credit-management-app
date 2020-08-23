e4a0c02d5f8e11ddfbfaa6c1d91492d66e84e210
Tarun <tarunsunny3@gmail.com>
2020-08-23 17:59:13 +0530

    Changed Index.js and build

---
+++ src/build/precache-manifest.8e76542ae2d3f780c1bbd0d7b4ef2e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e6b363a2b920eaeca0cfc6eeb2acae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87071e9741f4a337a5f3",
+    "url": "/static/css/main.29bccab3.chunk.css"
+  },
+  {
+    "revision": "0f8c2443097046459078",
+    "url": "/static/js/2.a22a6e05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a22a6e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87071e9741f4a337a5f3",
+    "url": "/static/js/main.32d9546e.chunk.js"
+  },
+  {
+    "revision": "13858d2f85791699b388",
+    "url": "/static/js/runtime-main.e646eca0.js"
+  }
+]);
