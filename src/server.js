d8aca015a4e88fabe9a37f96706c61c4e07669f0
Tarun <tarunsunny3@gmail.com>
2020-08-23 17:20:42 +0530

    Set engines

--- src/server.js
+++ src/server.js
@@ -96,6 +96,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 5000, (req, res) => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", (req, res) => {
   console.log("Server started successfully");
 });
